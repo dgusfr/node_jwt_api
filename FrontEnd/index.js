5dc2d7174c36bd2d11429592c1aa62b191104520
dgusfr <diegogustavo.fr@gmail.com>
2024-05-30 08:57:28 -0300

    changes

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -46,7 +46,7 @@ function loadGames() {
 document.addEventListener("DOMContentLoaded", loadGames);
 
 var axiosConfig = {
-  Headers: {
+  headers: {
     Authorization:
       "Bearer " +
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJ2aWN0b3JkZXZ0YkBndWlhZG9wcm9ncmFtYWRvci5jb20iLCJpYXQiOjE3MTcwNjk2NTAsImV4cCI6MTcxNzI0MjQ1MH0.8dc7kvTNyQtyM6tHRXguNi85LU_zMXo0o8hd4Ku7hms",
